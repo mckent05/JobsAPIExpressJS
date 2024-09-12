016683d7db3e13e2ef2f2b043426a0130dc6c2ae
mckent05 <atemitope95@gmail.com>
2024-09-12 22:45:08 +0100

    Added controllers to get job by ID and update job

--- Controllers/jobs.js
+++ Controllers/jobs.js
@@ -1,3 +1,4 @@
+const { BadRequestError, NotfoundError } = require("../Error")
 const Job = require("../model/jobs")
 const { StatusCodes } = require("http-status-codes")
 
@@ -14,8 +15,34 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({job})
 }
 
+const getJob = async (req, res) => {
+    const { user: { userId }, params: { id: jobId} } = req
+    const job = await Job.findOne({
+        _id: jobId,
+        userId
+    })
+    res.status(StatusCodes.OK).json({data: job})
+}
+
+const updateJob = async(req, res) => {
+    const { user: { userId }, params: { id: jobId}, body: {company, position } } = req
+    
+    if( company === "" || position === "") {
+        throw BadRequestError('Please add a value for both company and position')
+    }
+    const foundJob = await Job.findByIdAndUpdate({
+       _id: jobId, userId
+    }, req.body, {runValidators: true, new: true }
+    )
+    if(!foundJob) {
+        throw NotfoundError(`No job with id: ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({data: foundJob})
+}
+
 
 module.exports = {
     getAllJobs,
-    createJob
+    createJob,
+    getJob
 }
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const {
     getAllJobs,
-    createJob
+    createJob,
+    getJob
 } = require("../Controllers/jobs")
 
 const routes = express.Router()
 
-routes.route("/").get(getAllJobs)
+routes.route("/").get(getAllJobs).post(createJob)
 
-routes.route("/id").post(createJob)
+routes.route("/id").get(getJob)
 
 module.exports = routes
